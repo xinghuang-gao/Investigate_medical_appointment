6268b971ff6d5dafa9aa1fd970ff6ee279e22ab0
xinghuang-gao <47017982+xinghuang-gao@users.noreply.github.com>
2019-08-14 11:06:39 -0400

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,38 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# 
 # # Project: Investigate a Medical Appointment No Shows 2016 
-# 
-# ## Table of Contents
-# <ul>
-# <li><a href="#intro">Introduction</a></li>
-# <li><a href="#wrangling">Data Wrangling</a></li>
-# <li><a href="#eda">Exploratory Data Analysis</a></li>
-# <li><a href="#conclusions">Conclusions</a></li>
-# </ul>
-
-# <a id='intro'></a>
-# ## Introduction
-
-# The dataset collects information from 100k medical appointments in Brazill 2016. We focused on to understand why patients did not show up for their appointment. A number of characteristics about the patient are included in each row.
-# 
-# Given information and attributes about a patient, how likly a patient will show up and what the most important characteristics about patient are correlated with show-up appointment. Throughout the project, we will explore the data set to get answers and connect among the variables.
-# 
-# ### More specific questions:
-# 
-# 
-# **(1)** How many patients visited the hospital with multiple times?
-# 
-# **(2)** If there exists a correlation between patient with the scholarship and no-show frequence?
-# 
-# **(3)** If there exists a correlation between patient ages and no-show frequence?
-# 
-# **(4)** If there exists a correlation between aging and occurrences of health issues?
-# 
-
-# In[1]:
-
 
 # import statements for all of the packages that plan to use.
 # include a 'magic word' so that your visualizations are plotted
